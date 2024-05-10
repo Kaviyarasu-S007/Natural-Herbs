1f9c1c357fbce3072991542908eb521a59946bad
Kaviyarasu-S007 <kaviyarasushanthi2002@example.com>
2024-05-10 07:43:43 +0530

    login signup completed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./LoginPage.css";
 import { useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { FaEnvelope, FaFacebook } from '@fortawesome/free-brands-svg-icons'; 
+import { FaGoogle, FaFacebook, FaTwitter } from 'react-icons/fa';
 
 
 function LoginPage() {
@@ -29,10 +28,23 @@ function LoginPage() {
                         <br></br>
                         <input type={"submit"} className="submitButton" value="Login" onClick={handlesignuppage} />
                         <h5>Don't have an account?&nbsp;<a href="#" onClick={handlesignup} className="log">Signup</a></h5>
-                        {/* <FontAwesomeIcon icon={FaFacebook} /> */}
+                        
                     </form>
+                    
                 </div>
+                
+                
             </div>
+
+            <div className="icons">
+                <h4>Or</h4>
+                <a href="https://accounts.google.com/v3/signin/identifier?ifkv=AaSxoQxteF4Mw_J87_16QonWnCe3wJffn3dDyT9HUXTpmShCEVRH73k72pc2GiTr-wT6PEj1x18ePg&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-2146823079%3A1715306592988577&theme=mn&ddm=0" className="icon-link"><FaGoogle size={32} color="orange" className="google" /> </a>
+                <a href="https://www.facebook.com/" className="icon-link"><FaFacebook size={32} color="lightblue" className="facebook" /> </a>
+                <a href="https://twitter.com/i/flow/login" className="icon-link"><FaTwitter size={32} color="cyan" className="twitter" /> </a>
+            </div>
+
+            
+            
         </div>
     );
 }
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./SignupPage.css";
 import { useNavigate } from 'react-router-dom';
+import { FaGoogle, FaFacebook, FaTwitter } from 'react-icons/fa';
+
 
 function SignupPage() {
     const navigate = useNavigate();
@@ -18,7 +20,7 @@ function SignupPage() {
         <div className="container">
             <p className="title"><h4 className="text">Get Started</h4></p>
             <div className="underline"></div>
-            <div className="login">
+            <div className="sign">
                 <div className="inputs">
                     <form className="App">
                         <input type="text" placeholder="Name" className="inputField" />
@@ -31,6 +33,12 @@ function SignupPage() {
                         
                     </form>
                 </div>
+            </div>
+            <div className="icons">
+                <h4>Or</h4>
+                <a href="https://accounts.google.com/v3/signin/identifier?ifkv=AaSxoQxteF4Mw_J87_16QonWnCe3wJffn3dDyT9HUXTpmShCEVRH73k72pc2GiTr-wT6PEj1x18ePg&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-2146823079%3A1715306592988577&theme=mn&ddm=0" className="icon-link"><FaGoogle size={32} color="orange" className="google" /> </a>
+                <a href="https://www.facebook.com/" className="icon-link"><FaFacebook size={32} color="lightblue" className="facebook" /> </a>
+                <a href="https://twitter.com/i/flow/login" className="icon-link"><FaTwitter size={32} color="cyan" className="twitter" /> </a>
             </div>
         </div>
     );
